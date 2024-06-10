93bc5d5dfd80aef843bcbb601a89fa276259a370
desmond <dzycheong@gmail.com>
2024-06-10 23:29:11 +0800

    added debugger

--- nachos-4.0-final/code/userprog/userkernel.cc
+++ nachos-4.0-final/code/userprog/userkernel.cc
@@ -196,7 +196,7 @@ UserProgKernel::InitializeOneThread(char* name, int priority, int burst_time)
     t[threadNum]->space = new AddrSpace();
     t[threadNum]->setPriority(priority);
     t[threadNum]->setRemainingBurstTime(burst_time);
-    t[threadNum]->setRunTime(0);Å
+    t[threadNum]->setRunTime(0);
     t[threadNum]->setRRTime(0);
     t[threadNum]->setWaitTime(0);
 
